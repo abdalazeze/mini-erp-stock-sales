38c97e7a1ccd52afb7baeaae16151604a1465df9
Abdulaziz_Nour_Eddin <abd.alazeze@gmail.com>
2026-05-25 15:22:57 +0800

    fix: autoload language helper so lang() is available in views

---
+++ application/logs/log-2026-05-25.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2026-05-25 07:15:27 --> Config Class Initialized
+INFO - 2026-05-25 07:15:27 --> Hooks Class Initialized
+DEBUG - 2026-05-25 07:15:27 --> UTF-8 Support Enabled
+INFO - 2026-05-25 07:15:27 --> Utf8 Class Initialized
+INFO - 2026-05-25 07:15:27 --> URI Class Initialized
+INFO - 2026-05-25 07:15:27 --> Router Class Initialized
+INFO - 2026-05-25 07:15:27 --> Output Class Initialized
+INFO - 2026-05-25 07:15:27 --> Security Class Initialized
+DEBUG - 2026-05-25 07:15:27 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-05-25 07:15:27 --> CSRF cookie sent
+INFO - 2026-05-25 07:15:27 --> Input Class Initialized
+INFO - 2026-05-25 07:15:27 --> Language Class Initialized
+INFO - 2026-05-25 07:15:27 --> Loader Class Initialized
+INFO - 2026-05-25 07:15:27 --> Helper loaded: url_helper
+INFO - 2026-05-25 07:15:27 --> Helper loaded: form_helper
+INFO - 2026-05-25 07:15:27 --> Helper loaded: html_helper
+INFO - 2026-05-25 07:15:27 --> Helper loaded: app_helper
+INFO - 2026-05-25 07:15:27 --> Database Driver Class Initialized
+DEBUG - 2026-05-25 07:15:27 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2026-05-25 07:15:27 --> Session: Class initialized using 'files' driver.
+INFO - 2026-05-25 07:15:27 --> Form Validation Class Initialized
+INFO - 2026-05-25 07:15:27 --> Controller Class Initialized
+INFO - 2026-05-25 07:15:27 --> Language file loaded: language/english/ui_lang.php
+INFO - 2026-05-25 07:15:27 --> Model "User_model" initialized
+ERROR - 2026-05-25 07:15:27 --> Severity: error --> Exception: Call to undefined function lang() D:\Jobs\SmartLife\mini_erp_stock_sales\application\views\auth\login.php 10
